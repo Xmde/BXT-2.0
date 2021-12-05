61a0f696b3d3f2fd8b1313cdf2b104072e2f29fb
Xmde <erve2005@gmail.com>
2021-12-05 09:48:41 -0800

    updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
 	apps: [
 		{
-			name: 'BXT',
-			script: 'yarn start',
-			interpreter: 'bash',
+			name: 'BXT_Beta',
+			script: 'yarn',
+			args: ['start'],
+			instance_var: 'INSTANCE_ID',
 			watch: true,
 			restart_delay: 5000,
 			log_date_format: 'YYYY-MM-DDTHH:mm:ss',
+			post_update: ['yarn build'],
 		},
 	],
 };
